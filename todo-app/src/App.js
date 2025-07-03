e4041c01ca488ba6d6e801016d3f7750cc8d5022
Akshai Srinivasan <akshaisrin@gmail.com>
2025-07-03 00:25:50 -0700

    Pushed solutions for exercise 1

---
+++ todo-app/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>My To-Do List</h1>
+      </header>
+      <main className="App-main">
+        <div className="todo-container">
+          {/* Todo functionality will go here */}
+        </div>
+      </main>
+    </div>
+  );
+}
+
+export default App;
